c604ab2ba10da0d4370a872b7287274b863a153f
obijohnkanobi <119489622+obijohnkanobi@users.noreply.github.com>
2023-04-19 10:42:08 +0200

    færdig med: HomeController.java ProductRepository.java ConnectionManager.java Ændret ui.model import: import ch.qos.logback.core.model.Model;

--- src/main/java/com/example/wishlist/controller/HomeController.java
+++ src/main/java/com/example/wishlist/controller/HomeController.java
@@ -1,14 +1,15 @@
 package com.example.wishlist.controller;
 
-import ch.qos.logback.core.model.Model;
+
 import com.example.wishlist.model.Product;
 import com.example.wishlist.repository.ProductRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.ui.Model;
+
 
 @Controller
 public class HomeController {
